e3b0ecf175cde80779b35ce161f01e9bc023d3da
Nick Neos <nicholas.neos@gmail.com>
2020-09-08 22:29:30 +1000

    improve readability

--- apps/ZHAHueDimmerSwitch/zha_hue_dimmer_switch.py
+++ apps/ZHAHueDimmerSwitch/zha_hue_dimmer_switch.py
@@ -1,23 +1,6 @@
 """
 Customise the buttons on a Hue Dimmer Switch integrated with ZHA
-
-Customizable button events:
-    on_press
-    on_hold
-    on_short_release
-    on_long_release
-    up_press
-    up_hold
-    up_short_release
-    up_long_release
-    down_press
-    down_hold
-    down_short_release
-    down_long_release
-    off_press
-    off_hold
-    off_short_release
-    off_long_release
+https://github.com/nickneos/Appdaemon-ZHA-Hue-Dimmer-Switch
 """
 from appdaemon.plugins.hass.hassapi import Hass
 
@@ -26,21 +9,23 @@ class HueDimmerSwitch(Hass):
     """Define a Hue Dimmer Switch base feature"""
 
     def initialize(self):
-        """Configure"""
+        """Initialize AppDaemon App"""
 
+        # get config values
         self.light = self.args.get("light", None)
         self.switch = self.args.get("switch")
-        self.cycle_idx = -1 
-        
-        # get the advanced button config if specified
         self.button_config = self.args.get("advanced", None)
 
-        # get default button config if advanced config not specified
+        # integer counter used for cycle function
+        self.cycle_idx = -1
+
+        # check for basic config
         if not self.button_config:
             if not self.light:
-                self.log("Light entity not specified")
+                self.log("Light entity needs to be specified", level="ERROR")
                 return
-            
+
+            # default button config if advanced config not specified
             self.button_config = {
                 "on_short_release": {
                     "action_type": "cycle",
@@ -70,58 +55,66 @@ class HueDimmerSwitch(Hass):
 
         # listener for dimmer switch press
         self.listen_event(self.button_pressed_cb, "zha_event", device_ieee=self.switch)
-        
+
 
     def button_pressed_cb(self, event_name, data, kwargs):
         """Take action when button is pressed on dimmer switch."""
-        
+
+        # get button event
         button_name = data["command"]
+
+        # call action function if button event is defined in button config
         if button_name in self.button_config:
             self.action(self.button_config[button_name])
 
 
     def action(self, button_config):
         """Call the respective service based on the passed button config."""
+
+        # get values from button_config
         action_type = button_config["action_type"]
         entity = button_config["entity"]
         parameters = button_config.get("parameters", {})
-        
+
+        # handle cycle action
         if action_type == "cycle":
-            # handle cycle action
             parameters = [parameters] if type(parameters) is not list else parameters
             self.cycle_action(entity, parameters)
             return
-        elif action_type == "turn_off":
-            # set index back to -1 on turn_off
+
+        # reset index to -1 on turn_off
+        if action_type == "turn_off":
             self.cycle_idx = -1
 
         # lets do this
         self.call_service(
-            f"{entity.split('.')[0]}/{action_type}", 
-            entity_id=entity, 
+            f"{entity.split('.')[0]}/{action_type}",
+            entity_id=entity,
             **parameters
         )
+
 
     def cycle_action(self, light, param_list):
         """Cycle through the parameter list with each button press"""
 
+
+        # when index -1, turn on light with previous settings
         if self.cycle_idx == -1:
-            # when index -1, turn on light with previous settings
             parameters = {}
+        # otherwise get paramaters from list using index value
         else:
-            # otherwise get paramaters from list using index 
             try:
                 parameters = param_list[self.cycle_idx]
             except IndexError:
                 self.cycle_idx = 0
                 parameters = param_list[self.cycle_idx]
-        
+
         # lets do this
         self.call_service(
-            f"{light.split('.')[0]}/turn_on", 
-            entity_id=light, 
+            f"{light.split('.')[0]}/turn_on",
+            entity_id=light,
             **parameters
         )
-        
+
         # increment index for next button press
         self.cycle_idx += 1
